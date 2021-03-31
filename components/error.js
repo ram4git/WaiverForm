5ed8718d712318b6330154908fb3939aecbac504
Ram <irraju@gmail.com>
2021-03-31 11:47:58 -0500

    Fixed the typo

--- components/error.js
+++ components/error.js
@@ -4,7 +4,7 @@ export default () => {
       <div className="container transform skew-y-6 sm:skew-y-0 sm:rotate-2 max-w-4xl mx-auto shadow-2xl my-8 py-8 rounded-xl items-center  justify-center flex bg-white flex-col">
         <h1 className="text-center font-extrabold py-2 text-2xl text-red-800">
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-red-700 text-3xl">
-            We can't seem to find the page your are looking for
+            We can't find the page your are looking for
           </span>
         </h1>
         <img
